63c0bc4576b14fe003e8c9814da02a87ab0be9b7
francismsc <60035994+francismsc@users.noreply.github.com>
2023-05-10 15:14:33 +0100

    code cleaning

--- O_Foliao/Assets/Scripts/Events/PrimaryEventList.cs
+++ O_Foliao/Assets/Scripts/Events/PrimaryEventList.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-//using System.Runtime.CompilerServices;
 using UnityEngine;
 using static EventBaseData;
 using DayOfWeek = EventBaseData.DayOfWeek;
