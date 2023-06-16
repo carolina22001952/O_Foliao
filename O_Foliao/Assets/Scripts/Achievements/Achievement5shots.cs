960a554b5f9564e32546c3bb016e39233a47ff9c
francismsc <frankymccosta@gmail.com>
2023-06-16 21:00:16 +0100

    Achievements barmen

---
+++ O_Foliao/Assets/Scripts/Achievements/Achievement5shots.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement5shots : MonoBehaviour, IAchievements
+{
+    [SerializeField]
+    private string achievementName;
+
+    [SerializeField]
+    private bool isUnlocked;
+
+    [SerializeField]
+    private Image achievementSprite;
+    [SerializeField]
+    private Sprite completedAchievementSprite;
+
+    private int drinkedShots = 0;
+
+    public void AchievementUnlock()
+    {
+        if (!isUnlocked)
+        {
+            isUnlocked = true;
+            AchievementUI();
+        }
+    }
+
+    public void AchievementLogic()
+    {
+        drinkedShots++;
+        if (drinkedShots == 5)
+        {
+            AchievementUnlock();
+        }
+       
+    }
+
+    public void AchievementUI()
+    {
+        achievementSprite.sprite = completedAchievementSprite;
+    }
+
+    public string GetAchievementName()
+    {
+        return achievementName;
+    }
+}
---
+++ O_Foliao/Assets/Scripts/Achievements/AchievementJantar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementJantar : MonoBehaviour, IAchievements
+{
+    [SerializeField]
+    private string achievementName;
+
+    [SerializeField]
+    private bool isUnlocked;
+
+    [SerializeField]
+    private Image achievementSprite;
+    [SerializeField]
+    private Sprite completedAchievementSprite;
+
+    public void AchievementUnlock()
+    {
+        if (!isUnlocked)
+        {
+            isUnlocked = true;
+            AchievementUI();
+        }
+    }
+
+    public void AchievementLogic()
+    {
+        AchievementUnlock();
+    }
+
+    public void AchievementUI()
+    {
+        achievementSprite.sprite = completedAchievementSprite;
+    }
+
+    public string GetAchievementName()
+    {
+        return achievementName;
+    }
+}
