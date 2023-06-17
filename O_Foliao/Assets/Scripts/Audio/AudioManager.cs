2236d9675adb4e1c9dc0d645ecde25af8b5cf155
Carolina Bastos <carol1999b@gmail.com>
2023-06-17 15:15:34 +0100

    Audio

--- O_Foliao/Assets/Scripts/Audio/AudioManager.cs
+++ O_Foliao/Assets/Scripts/Audio/AudioManager.cs
@@ -9,10 +9,15 @@ public class AudioManager : MonoBehaviour
 
     [Header("Random Sounds")]
     [SerializeField] AudioClip[] clips;
-    //[SerializeField] private AudioSource megaphoneSound;
-    //[SerializeField] private AudioSource whistleSound;
-    //[SerializeField] private AudioSource Sound;
 
+    private int clipIndex;
+    [SerializeField] private AudioSource audioSource;
+    //private bool audioPlaying = false;
+
+    void Start()
+    {
+        StartCoroutine(PlayRandomSound());
+    }
 
     public void PlayButtonSound()
     {
@@ -22,5 +27,16 @@ public class AudioManager : MonoBehaviour
     public void PlayNotificationSound()
     {
         notificationSound.Play();
+    }
+
+    IEnumerator PlayRandomSound()
+    {
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+
+        clipIndex = Random.Range(0, clips.Length);
+        audioSource.PlayOneShot(clips[clipIndex], 1f);
+
+        yield return new WaitForSeconds(clips[clipIndex].length);
+        StartCoroutine(PlayRandomSound());
     }
 }
